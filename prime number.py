e2fba295863aa27234bd16fa89a516f6c4f052cb
cw0512 <lcw00017@gmail.com>
2020-06-29 23:06:21 +0900

    6/29

---
+++ prime number.py
@@ -0,0 +1,11 @@
+n = 600851475143
+
+for i in range(2, n + 1) :
+    cnt = 0
+
+    for j in range(2, i) :
+        if i % j == 0 :
+           cnt = 1
+
+    if cnt == 0 :
+        print(i)
---
+++ projectEuler/4.py
@@ -0,0 +1,25 @@
+#앞에서부터 읽을 때나 뒤에서부터 읽을 때나 모양이 같은 수를 대칭수(palindrome)라고 부릅니다.
+
+#두 자리 수를 곱해 만들 수 있는 대칭수 중 가장 큰 수는 9009 (= 91 × 99) 입니다.
+
+#세 자리 수를 곱해 만들 수 있는 가장 큰 대칭수는 얼마입니까?
+
+#세 자리 수 X 세 자리 수 = 다섯 자리 수, 여섯 자리 수
+
+#  다섯 자리 수 일 때 abbba, abcba, ababa, aabaa
+#  316/317 이 경계
+#  여섯 자리 수 일 때 abccba, abbbba, abaaba, aaaaaa, aabbaa 총 다섯가지 형태 존재함
+
+#5
+#abbba
+#ababa
+#abcba
+#aabaa
+
+
+#6
+# 1. abccba 
+# 2. abbbba
+# 3. abaaba
+# 4. aaaaaa
+# 5. aabbaa
